769059663956bfefce79087c749e065148347ef6
buzhi <1716496537@qq.com>
2022-12-12 20:14:30 +0800

    12.9任务 day01

---
+++ 12.9任务/demo-项目配置/src/views/AboutView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="about">
+    <h1>This is an about page</h1>
+		<h2>{{msg}}</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+	data(){
+		return {
+			msg:"about信息"
+		}
+	}
+}
+</script>
